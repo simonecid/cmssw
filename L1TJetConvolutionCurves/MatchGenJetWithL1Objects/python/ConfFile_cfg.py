bbfe5b13642d0ab68a0881ba2606f78c6c864921
Simone Bologna <smnbologna@gmail.com>
2017-07-19 09:09:55 +0200

    Splitted file in samples + cfg

--- L1TJetConvolutionCurves/MatchGenJetWithL1Objects/python/ConfFile_cfg.py
+++ L1TJetConvolutionCurves/MatchGenJetWithL1Objects/python/ConfFile_cfg.py
@@ -1,12 +1,28 @@
 import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+from L1TJetConvolutionCurves.MatchGenJetWithL1Objects.source_QCD_Pt_15_3000_splitted import *
 
 process = cms.Process("MatchGenJetWithL1Objects")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+options = VarParsing.VarParsing ('analysis')
+options.register ('source',
+                  "", # default value
+                  VarParsing.VarParsing.multiplicity.singleton, # singleton or list
+                  VarParsing.VarParsing.varType.string,          # string, int, or float
+                  "Source sample")
+options.outputFile = 'l1tObjectGenJetMatching.root'
+options.source = source_0
+options.parseArguments()
 
-process.TFileService = cms.Service("TFileService", fileName = cms.string("l1tObjectGenJetMatching.root") )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+if options.source:
+  process.source = locals()[options.source]
+
+process.TFileService = cms.Service('TFileService', fileName = cms.string(options.outputFile))
 
 process.MatchGenJetWithL1Objects = cms.EDAnalyzer('MatchGenJetWithL1Objects',
   genJetCollectionTag = cms.InputTag("ak4GenJets"),
@@ -16,6 +32,4 @@ process.MatchGenJetWithL1Objects = cms.EDAnalyzer('MatchGenJetWithL1Objects',
   l1tEGammaCollectionTag = cms.InputTag("simCaloStage2Digis"),
 )
 
-process.source = source_QCD_Pt_15_3000
-
-process.p = cms.Path(process.MatchGenJetWithL1Objects)
+process.p = cms.Path(process.MatchGenJetWithL1Objects)  
