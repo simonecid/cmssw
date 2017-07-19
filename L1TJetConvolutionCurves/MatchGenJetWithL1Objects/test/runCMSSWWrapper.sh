221daed0067e581129ca170ead22c7efdf0aa9a6
Simone Bologna <smnbologna@gmail.com>
2017-07-19 15:23:53 +0100

    Changed shell

--- L1TJetConvolutionCurves/MatchGenJetWithL1Objects/test/runCMSSWWrapper.sh
+++ L1TJetConvolutionCurves/MatchGenJetWithL1Objects/test/runCMSSWWrapper.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/sh
 
 #set -o xtrace
 
@@ -31,15 +31,15 @@ echo "Running CMSSW job"
 
 HOME_FOLDER="$(pwd)"
 OUTPUT_FILENAME=${jobName}_${sampleName}_${clusterId}.${processId}.root
-
 source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+set -o xtrace
 cp -r /software/sb17498/CMSSW_9_0_0 .
 cd CMSSW_9_0_0/src
 cmsenv
 scramv1 b ProjectRename
 scram b
 
-set -o xtrace
 cmsRun ${inputFile} source=source_${processId} outputFile=${OUTPUT_FILENAME}
 
 echo "Will save on" /FCC-hh/${HDFS_DEST}
