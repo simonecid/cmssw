c7b5750e725a6f53b79ce4ad8962f804c7f2e313
Simone Bologna <smnbologna@gmail.com>
2018-06-25 10:20:39 +0100

    Base code

--- L1TJetConvolutionCurves/MatchL1TMuonWithGenLevelMuons/python/ConfFile_cfg.py
+++ L1TJetConvolutionCurves/MatchL1TMuonWithGenLevelMuons/python/ConfFile_cfg.py
@@ -4,6 +4,11 @@ from importlib import import_module
 
 process = cms.Process("MatchL1TMuonWithGenLevelMuons")
 
+process.load('Configuration.Geometry.GeometryExtended2023D4Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
@@ -20,7 +25,7 @@ options.register ('sourceFile',
                   "File containing the splitted sources")
 options.outputFile = 'MatchL1TMuonWithGenLevelMuons_GluGlu_HToMuMu.root'
 options.source = "source_0"
-options.sourceFile = "source_SingleNeutrinoPU140_splitted"
+options.sourceFile = "source_QCD_Pt_15_3000_splitted"
 
 options.parseArguments()
 
@@ -31,9 +36,26 @@ process.source = getattr(import_module("L1TJetConvolutionCurves.MatchGenJetWithL
 process.TFileService = cms.Service('TFileService', fileName = cms.string(options.outputFile))
 
 process.MatchL1TMuonWithGenLevelMuons = cms.EDAnalyzer('MatchL1TMuonWithGenLevelMuons',
-  genParticleCollectionTag = cms.InputTag("genParticles"),
+  genParticleCollectionTag = cms.InputTag( "PropagateGenMuonAndGenJetsTo2ndMuonStation", "PropagatedGenMuons", "MatchGenJetWithL1Objects"),
   l1tMuonCollectionTag = cms.InputTag("simGmtStage2Digis"),
 )
 
+process.GenMuonCollectionProducer = cms.EDProducer('GenMuonCollectionProducer',
+  genParticleCollectionTag = cms.InputTag("genParticles"),
+)
 
-process.p = cms.Path(process.MatchL1TMuonWithGenLevelMuons)
+process.PropagateGenMuonAndGenJetsTo2ndMuonStation = cms.EDProducer('PropagateGenMuonAndGenJetsTo2ndMuonStation',
+  genParticleCollectionTag = cms.InputTag("genParticles"),
+  genJetCollectionTag = cms.InputTag("ak4GenJets"),
+  prop2nd = cms.PSet(
+    useTrack = cms.string("none"),  # 'none' to use Candidate P4; or 'tracker', 'muon', 'global'
+    useState = cms.string("atVertex"), # 'innermost' and 'outermost' require the TrackExtra
+    useSimpleGeometry = cms.bool(True),
+    useStation2 = cms.bool(True),
+    fallbackToME1 = cms.bool(False)
+  )
+)
+
+
+process.p = cms.Path(process.PropagateGenMuonAndGenJetsTo2ndMuonStation *
+                     process.MatchL1TMuonWithGenLevelMuons)
