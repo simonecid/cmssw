ffb0dc730ae351a67633e3755dd81366ac060709
Simone Bologna <smnbologna@gmail.com>
2018-06-26 13:15:05 +0100

    Set SCRAM_ARCH

--- L1TJetConvolutionCurves/MatchL1TMuonWithGenLevelMuons/test/runCMSSWWrapper.sh
+++ L1TJetConvolutionCurves/MatchL1TMuonWithGenLevelMuons/test/runCMSSWWrapper.sh
@@ -30,6 +30,8 @@ HOME_FOLDER="$(pwd)"
 OUTPUT_FILENAME=${jobName}_${clusterId}.${processId}.root
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
+export SCRAM_ARCH=slc6_amd64_gcc630
+
 set -o xtrace
 cmsrel CMSSW_9_0_0
 cd CMSSW_9_0_0/src
