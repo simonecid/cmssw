5e42b776113a717963ab62cd87b9f67b66d7df8d
Simone Bologna <smnbologna@gmail.com>
2018-08-03 16:50:48 +0100

    Imported gen-jet matched from FCC matching code

---
+++ TriggerPerformanceAnalysis/MatchGenJetWithL1Objects/test/runCMSSWWrapper.sh
@@ -0,0 +1,56 @@
+#! /bin/sh
+
+#set -o xtrace
+
+export HOME=/users/sb17498
+
+while getopts "j:c:p:i:d:s:b:" o; do
+  case "${o}" in
+    j)
+      jobName=${OPTARG}
+      ;;
+    c)
+      clusterId=${OPTARG}
+      ;;
+    p)
+      processId=${OPTARG}
+      ;;  
+    i)
+      inputFile=${OPTARG}
+      ;;
+    d)
+      HDFS_DEST=${OPTARG}
+      ;;
+    s)
+      SAMPLE_FILE=${OPTARG}
+      ;;
+    b)
+      BRANCH=${OPTARG}
+    esac
+done
+
+export SCRAM_ARCH=slc6_amd64_gcc530
+
+echo "I am running on" $HOSTNAME
+
+echo "Running CMSSW job"
+
+HOME_FOLDER="$(pwd)"
+OUTPUT_FILENAME=${jobName}_${clusterId}.${processId}.root
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+set -o xtrace
+cmsrel CMSSW_9_0_0
+cd CMSSW_9_0_0/src
+cmsenv
+git cms-merge-topic simonecid:${BRANCH}
+scram b
+
+cmsRun ${inputFile} source=source_${processId} outputFile=${OUTPUT_FILENAME} sourceFile=${SAMPLE_FILE}
+
+echo "Will save on" /FCC-hh/${HDFS_DEST}
+
+/usr/bin/hdfs dfs -mkdir -p /FCC-hh/${HDFS_DEST}
+/usr/bin/hdfs dfs -moveFromLocal ${OUTPUT_FILENAME} /FCC-hh/${HDFS_DEST}
+
+set +o xtrace
