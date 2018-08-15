f0f0196b519b88967484f2328b0ecdb2bc92d13a
Simone Bologna <smnbologna@gmail.com>
2018-08-15 11:56:55 +0100

    Added AK4 on L1PfClusters/Candidates

--- L1Trigger/L1CaloTrigger/python/test_L1TJetPhase1Producer_cfg.py
+++ L1Trigger/L1CaloTrigger/python/test_L1TJetPhase1Producer_cfg.py
@@ -2,6 +2,12 @@ import FWCore.ParameterSet.Config as cms
 from L1Trigger.L1CaloTrigger.caloEtaSegmentation import caloEtaSegmentation
 from math import pi
 
+
+import FWCore.ParameterSet.Config as cms
+
+from RecoJets.JetProducers.GenJetParameters_cfi import *
+from RecoJets.JetProducers.AnomalousCellParameters_cfi import *
+
 process = cms.Process("L1TJetPhase1Producer")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
@@ -16,9 +22,20 @@ process.source = process.source = cms.Source("PoolSource",
 process.TFileService = cms.Service('TFileService', fileName = cms.string("Histograms.root"))
 #process.source.skipEvents = cms.untracked.uint32(1)
 
+GenJetParameters.src = cms.InputTag("pfClustersFromCombinedCalo", "calibrated", "IN")
+#GenJetParameters.src = cms.InputTag("genParticles", "", "HLT")
+
+process.FastjetJetProducer = cms.EDProducer(
+    "FastjetJetProducer",
+    GenJetParameters,
+    AnomalousCellParameters,
+    jetAlgorithm = cms.string("AntiKt"),
+    rParam       = cms.double(0.4)
+)
+
 process.L1TJetPhase1Producer = cms.EDProducer('L1TJetPhase1Producer',
-  pfCandidateCollectionTag = cms.InputTag("l1pfProducer", "Puppi", "IN"),
-  pfClusterCollectionTag = cms.InputTag("pfClustersFromCombinedCalo", "uncalibrated", "IN"),
+  pfCandidateCollectionTag = cms.InputTag("l1pfProducerTightTK", "Puppi", "IN"),
+  pfClusterCollectionTag = cms.InputTag("pfClustersFromCombinedCalo", "calibrated", "IN"),
   etaBinning = caloEtaSegmentation,
   nBinsPhi = cms.uint32(72),
   phiLow = cms.double(-pi),
@@ -28,12 +45,18 @@ process.L1TJetPhase1Producer = cms.EDProducer('L1TJetPhase1Producer',
   seedPtThreshold = cms.double(4) # GeV
 )
 
-process.MatchGenJetWithL1Objects = cms.EDAnalyzer('MatchGenJetWithL1Objects',
-  genJetCollectionTag = cms.InputTag("ak4GenJetsNoNu", "", "HLT"),
-  l1tJetCollectionTag = cms.InputTag("L1TJetPhase1Producer", "Phase1L1TJetFromPfCandidates", "L1TJetPhase1Producer"),
+process.PrintMomentum = cms.EDAnalyzer('PrintMomentum',
+  genJetCollectionTag = cms.InputTag("FastjetJetProducer", "", "L1TJetPhase1Producer"),
+  phase1L1TJetFromPfClustersTag = cms.InputTag("L1TJetPhase1Producer", "Phase1L1TJetFromPfClusters", "L1TJetPhase1Producer"),
 )
 
-process.p = cms.Path(process.L1TJetPhase1Producer + process.MatchGenJetWithL1Objects)
+process.MatchGenJetWithL1Objects = cms.EDAnalyzer('MatchGenJetWithL1Objects',
+  genJetCollectionTag = cms.InputTag("ak4GenJetsNoNu", "", "HLT"),
+  l1tJetCollectionTag = cms.InputTag("L1TJetPhase1Producer", "Phase1L1TJetFromPfClusters", "L1TJetPhase1Producer"),
+)
+
+#process.p = cms.Path(process.FastjetJetProducer + process.L1TJetPhase1Producer + process.MatchGenJetWithL1Objects)
+process.p = cms.Path(process.FastjetJetProducer + process.L1TJetPhase1Producer + process.PrintMomentum)
 
 process.out = cms.OutputModule("PoolOutputModule",
   fileName = cms.untracked.string('myOutputFile.root')
