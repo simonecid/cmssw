e6b387f6068c98474acc62536c54666d676abc6c
Simone Bologna <smnbologna@gmail.com>
2018-09-06 14:18:32 +0100

    Added wrapper script for condor

---
+++ runCMSSWWrapper.sh
@@ -0,0 +1,62 @@
+#! /bin/sh
+
+#set -o xtrace
+
+export HOME=/users/sb17498
+
+while getopts "j:c:p:i:n:d:s:b:" o; do
+  case "${o}" in
+    j)
+      jobName=${OPTARG}
+      ;;
+    c)
+      clusterId=${OPTARG}
+      ;;
+    p)
+      processId=${OPTARG}
+      ;;
+    n) 
+      numberOfJobs=${OPTARG}
+      ;;
+    i)
+      inputFile=${OPTARG}
+      ;;
+    d)
+      HDFS_DEST=${OPTARG}
+      ;;
+    s)
+      SAMPLE_FILE=${OPTARG}
+      ;;
+    b)
+      BRANCH=${OPTARG}
+    esac
+done
+
+export SCRAM_ARCH=slc6_amd64_gcc630
+
+echo "I am running on" $HOSTNAME
+
+echo "Running CMSSW job"
+
+HOME_FOLDER="$(pwd)"
+OUTPUT_FILENAME=${jobName}_${clusterId}.${processId}.root
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+set -o xtrace
+cmsrel CMSSW_10_1_5
+rm -r CMSSW_10_1_5/src
+cp -r /software/sb17498/CMSSW_10_1_5/src CMSSW_10_1_5
+cd CMSSW_10_1_5/src
+cmsenv
+scramv1 b ProjectRename
+#git cms-merge-topic simonecid:${BRANCH}
+scram b -j4
+
+cmsRun CMSSWJobDriver.py blockIndex=${processId} numberOfBlocks=${numberOfJobs} outputFile=${OUTPUT_FILENAME} cfgFile=${inputFile} inputFiles=${SAMPLE_FILE}
+
+echo "Will save on" /user/sb17498/CMS_Phase_2/jetMETStudies/${HDFS_DEST}
+
+/usr/bin/hdfs dfs -mkdir -p /user/sb17498/CMS_Phase_2/jetMETStudies/${HDFS_DEST}
+/usr/bin/hdfs dfs -moveFromLocal ${OUTPUT_FILENAME} /user/sb17498/CMS_Phase_2/jetMETStudies/${HDFS_DEST}
+
+set +o xtrace
