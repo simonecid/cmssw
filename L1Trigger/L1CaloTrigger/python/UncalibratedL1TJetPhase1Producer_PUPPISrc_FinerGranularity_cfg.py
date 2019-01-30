74819f5fff297f3b5ae075e450a1dc8b2b10c971
Simone Bologna <smnbologna@gmail.com>
2019-01-30 16:26:13 +0000

    Added finer granularity and binning stuff

---
+++ L1Trigger/L1CaloTrigger/python/UncalibratedL1TJetPhase1Producer_PUPPISrc_FinerGranularity_cfg.py
@@ -0,0 +1,5 @@
+from L1Trigger.L1CaloTrigger.UncalibratedL1TJetPhase1Producer_FinerGranularity_cfg import *
+
+ak4JetFromPfClustersParameters.src = cms.InputTag("pfClustersFromCombinedCalo", "calibrated", "IN")
+ak4JetFromPfCandidatesParameters.src = cms.InputTag("l1pfProducer", "Puppi", "IN")
+
