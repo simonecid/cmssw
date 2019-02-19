487981119c97b86546dd3fb6c65b2de3f58b6c1f
Simone Bologna <smnbologna@gmail.com>
2019-02-19 09:27:20 +0000

    Updated paths

--- JetCalibration/ApplyCalibrationFactors/python/BuildCalibrationVPSet.py
+++ JetCalibration/ApplyCalibrationFactors/python/BuildCalibrationVPSet.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import FWCore.ParameterSet.Config as cms
 import os
 import pickle as pkl
@@ -39,8 +40,19 @@ def BuildCalibrationVPSet(rootfile_path, etaBinning, outputfile_path):
 
 
 if __name__ == "__main__":
-  pass
-    #BuildCalibrationVPSet("JetCalibration/Calibration_MatchAK4GenJetWithAK4JetFromPfCandidates_PU200_NoZeroPtJets_FinerGranularity.root", [0, 1.4, 3, 5.191])
-    #BuildCalibrationVPSet("JetCalibration/Calibration_MatchAK4GenJetWithAK4JetFromPfClusters_PU200_NoZeroPtJets_FinerGranularity.root", [0, 1.4, 3, 5.191])
-    #BuildCalibrationVPSet("JetCalibration/Calibration_MatchAK4GenJetWithPhase1L1TJetFromPfCandidates_PU200_NoZeroPtJets_FinerGranularity.root", [0, 1.4, 3, 5.191])
-    #BuildCalibrationVPSet("JetCalibration/Calibration_MatchAK4GenJetWithPhase1L1TJetFromPfClusters_PU200_NoZeroPtJets_FinerGranularity.root", [0, 1.4, 3, 5.191])
+  BuildCalibrationVPSet("Calibration_Tree_Calibration_QCD_PU200_Puppi_NoZeroPTJets_0.4Square_MatchAK4GenJetWithAK4JetFromPfCandidates_3752475.0.root",
+  [0, 0.435, 0.783, 1.131, 1.305, 1.479, 1.653, 1.83, 1.93, 2.043, 2.172, 2.322, 2.5, 2.964, 3.489, 4.191, 5.191],
+  "Calibration_Tree_Calibration_QCD_PU200_Puppi_NoZeroPTJets_0.4Square_MatchAK4GenJetWithAK4JetFromPfCandidates_3752475.0.pickle"
+  )
+  BuildCalibrationVPSet("Calibration_Tree_Calibration_QCD_PU200_Puppi_NoZeroPTJets_0.4Square_MatchAK4GenJetWithAK4JetFromPfClusters_3752474.0.root",
+  [0, 0.435, 0.783, 1.131, 1.305, 1.479, 1.653, 1.83, 1.93, 2.043, 2.172, 2.322, 2.5, 2.964, 3.489, 4.191, 5.191],
+  "Calibration_Tree_Calibration_QCD_PU200_Puppi_NoZeroPTJets_0.4Square_MatchAK4GenJetWithAK4JetFromPfClusters_3752474.0.pickle"
+  )
+  BuildCalibrationVPSet("Calibration_Tree_Calibration_QCD_PU200_Puppi_NoZeroPTJets_0.4Square_MatchAK4GenJetWithPhase1L1TJetFromPfCandidates_3752477.0.root",
+  [0, 0.435, 0.783, 1.131, 1.305, 1.479, 1.653, 1.83, 1.93, 2.043, 2.172, 2.322, 2.5, 2.964, 3.489, 4.191, 5.191],
+  "Calibration_Tree_Calibration_QCD_PU200_Puppi_NoZeroPTJets_0.4Square_MatchAK4GenJetWithPhase1L1TJetFromPfCandidates_3752477.0.pickle"
+  )
+  BuildCalibrationVPSet("Calibration_Tree_Calibration_QCD_PU200_Puppi_NoZeroPTJets_0.4Square_MatchAK4GenJetWithPhase1L1TJetFromPfClusters_3752476.0.root",
+  [0, 0.435, 0.783, 1.131, 1.305, 1.479, 1.653, 1.83, 1.93, 2.043, 2.172, 2.322, 2.5, 2.964, 3.489, 4.191, 5.191],
+  "Calibration_Tree_Calibration_QCD_PU200_Puppi_NoZeroPTJets_0.4Square_MatchAK4GenJetWithPhase1L1TJetFromPfClusters_3752476.0.pickle"
+  )
