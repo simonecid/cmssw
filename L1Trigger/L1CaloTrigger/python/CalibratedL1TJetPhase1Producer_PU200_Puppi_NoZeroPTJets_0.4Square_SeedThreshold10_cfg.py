e4d1d6e508b2d6441d4a4b7b0727343697b46a16
Simone Bologna <smnbologna@gmail.com>
2019-03-08 17:47:19 +0000

    Added a high-threshold tune

---
+++ L1Trigger/L1CaloTrigger/python/CalibratedL1TJetPhase1Producer_PU200_Puppi_NoZeroPTJets_0.4Square_SeedThreshold10_cfg.py
@@ -0,0 +1,16 @@
+from L1Trigger.L1CaloTrigger.CalibratedL1TJetPhase1Producer_cfg import *
+
+CalibrateAK4JetFromPfClusters_path = "/hdfs/user/sb17498/CMS_Phase_2/jetMETStudies/CalibrationFactors/Calibration_Tree_Calibration_QCD_PU200_Puppi_NoZeroPTJets_0.4Square_SeedThreshold10_MatchAK4GenJetWithAK4JetFromPfClusters_3753203.pickle"
+CalibrateAK4JetFromPfCandidates_path = "/hdfs/user/sb17498/CMS_Phase_2/jetMETStudies/CalibrationFactors/Calibration_Tree_Calibration_QCD_PU200_Puppi_NoZeroPTJets_0.4Square_SeedThreshold10_MatchAK4GenJetWithAK4JetFromPfCandidates_3753204.pickle"
+CalibratePhase1L1TJetFromPfClusters_path = "/hdfs/user/sb17498/CMS_Phase_2/jetMETStudies/CalibrationFactors/Calibration_Tree_Calibration_QCD_PU200_Puppi_NoZeroPTJets_0.4Square_SeedThreshold10_MatchAK4GenJetWithPhase1L1TJetFromPfClusters_3753205.pickle"
+CalibratePhase1L1TJetFromPfCandidates_path = "/hdfs/user/sb17498/CMS_Phase_2/jetMETStudies/CalibrationFactors/Calibration_Tree_Calibration_QCD_PU200_Puppi_NoZeroPTJets_0.4Square_SeedThreshold10_MatchAK4GenJetWithPhase1L1TJetFromPfCandidates_3753206.pickle"
+
+
+with open(CalibratePhase1L1TJetFromPfClusters_path, 'rb') as f:
+  process.CalibratePhase1L1TJetFromPfClusters.calibration = pkl.load(f)
+with open(CalibratePhase1L1TJetFromPfCandidates_path, 'rb') as f:
+  process.CalibratePhase1L1TJetFromPfCandidates.calibration = pkl.load(f)
+with open(CalibrateAK4JetFromPfClusters_path, 'rb') as f:
+  process.CalibrateAK4JetFromPfClusters.calibration = pkl.load(f)
+with open(CalibrateAK4JetFromPfCandidates_path, 'rb') as f:
+  process.CalibrateAK4JetFromPfCandidates.calibration = pkl.load(f)
