5ac7ce2af407485fd4fe97b825522b04ea30e104
Simone Bologna <smnbologna@gmail.com>
2019-11-07 14:39:37 +0000

    Added MET and HT

--- L1Trigger/L1CaloTrigger/test/test_Phase1L1TJets_cfg.py
+++ L1Trigger/L1CaloTrigger/test/test_Phase1L1TJets_cfg.py
@@ -7,19 +7,34 @@ process = cms.Process("TEST")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(15000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
-fileList = FileUtils.loadListFromFile('ttbar.list')
-readFiles = cms.untracked.vstring(*fileList)
+# fileList = FileUtils.loadListFromFile('ttbar.list')
+# readFiles = cms.untracked.vstring(*fileList)
 
-process.source = process.source = cms.Source("PoolSource",
-  fileNames = readFiles,
+process.source = cms.Source("PoolSource",
+  fileNames = cms.untracked.vstring("file:/hdfs/user/sb17498/CMS_Phase_2/jetMETStudies/QCD_Pt-15To7000_PU200/inputs104X_71.root"),
   #fileNames = cms.untracked.vstring(
   #  "file:pf500.root",
   #)
 )
 
 process.load('L1Trigger.L1CaloTrigger.Phase1L1TJets_cff')
+
+# process.out = cms.OutputModule("PoolOutputModule",
+#   fileName = cms.untracked.string('myOutputFile.root'),
+#   outputCommands = cms.untracked.vstring(
+#     "drop *",
+#     "keep *_Phase1L1TJetProducer_*_*",
+#     "keep *_ak4GenJetsNoNu_*_*",
+#     "keep *_Phase1L1TJetCalibrator_*_*",
+#   ),
+# )
+
+# process.p = cms.Path(process.Phase1L1TJetsSequence)
+
+# process.e = cms.EndPath(process.out)
+
 
 process.out = cms.OutputModule("PoolOutputModule",
   fileName = cms.untracked.string('myOutputFile.root'),
@@ -29,6 +44,12 @@ process.out = cms.OutputModule("PoolOutputModule",
     "keep *_ak4GenJetsNoNu_*_*",
     "keep *_Phase1L1TJetCalibrator_*_*",
   ),
+)
+
+from L1Trigger.L1CaloTrigger.Phase1L1TJetProducer_cfi import Phase1L1TJetProducer
+
+Phase1L1TJetsSequence = cms.Sequence(
+  Phase1L1TJetProducer
 )
 
 process.p = cms.Path(process.Phase1L1TJetsSequence)
