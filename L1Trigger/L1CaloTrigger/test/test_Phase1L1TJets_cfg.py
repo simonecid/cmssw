948991625ece4e77ad31aee7f96d2f330a73d366
Simone Bologna <smnbologna@gmail.com>
2019-11-29 10:54:02 +0000

    Move HT/MET impl. to another plugin

--- L1Trigger/L1CaloTrigger/test/test_Phase1L1TJets_cfg.py
+++ L1Trigger/L1CaloTrigger/test/test_Phase1L1TJets_cfg.py
@@ -21,6 +21,29 @@ process.source = cms.Source("PoolSource",
 
 process.load('L1Trigger.L1CaloTrigger.Phase1L1TJets_cff')
 
+process.out = cms.OutputModule("PoolOutputModule",
+  fileName = cms.untracked.string('myOutputFile.root'),
+  outputCommands = cms.untracked.vstring(
+    "drop *",
+    "keep *_Phase1L1TJetProducer_*_*",
+    "keep *_ak4GenJetsNoNu_*_*",
+    "keep *_Phase1L1TJetCalibrator_*_*",
+    "keep *_Phase1L1TSumsProducer_*_*",
+  ),
+)
+
+# process.SaveSums = cms.EDProducer("SaveGenSumsAndL1Sums",
+#   genMETCollectionTag = cms.InputTag("", "", "")
+#   l1tMETCollectionTag = cms.InputTag("", "", "")
+#   genJetCollectionTag = cms.InputTag("", "", "")
+#   l1tHTCollectionTag = cms.InputTag("", "", "")
+# )
+
+process.p = cms.Path(process.Phase1L1TJetsSequence )
+
+process.e = cms.EndPath(process.out)
+
+
 # process.out = cms.OutputModule("PoolOutputModule",
 #   fileName = cms.untracked.string('myOutputFile.root'),
 #   outputCommands = cms.untracked.vstring(
@@ -31,27 +54,12 @@ process.load('L1Trigger.L1CaloTrigger.Phase1L1TJets_cff')
 #   ),
 # )
 
+# from L1Trigger.L1CaloTrigger.Phase1L1TJetProducer_cfi import Phase1L1TJetProducer
+
+# Phase1L1TJetsSequence = cms.Sequence(
+#   Phase1L1TJetProducer
+# )
+
 # process.p = cms.Path(process.Phase1L1TJetsSequence)
 
 # process.e = cms.EndPath(process.out)
-
-
-process.out = cms.OutputModule("PoolOutputModule",
-  fileName = cms.untracked.string('myOutputFile.root'),
-  outputCommands = cms.untracked.vstring(
-    "drop *",
-    "keep *_Phase1L1TJetProducer_*_*",
-    "keep *_ak4GenJetsNoNu_*_*",
-    "keep *_Phase1L1TJetCalibrator_*_*",
-  ),
-)
-
-from L1Trigger.L1CaloTrigger.Phase1L1TJetProducer_cfi import Phase1L1TJetProducer
-
-Phase1L1TJetsSequence = cms.Sequence(
-  Phase1L1TJetProducer
-)
-
-process.p = cms.Path(process.Phase1L1TJetsSequence)
-
-process.e = cms.EndPath(process.out)
