33410040ac7912e676010feb414c956c3ee2b62b
Simone Bologna <smnbologna@gmail.com>
2020-04-27 14:16:28 +0100

    Patched pattern writer configuration2

--- L1Trigger/L1CaloTrigger/python/l1tS2PFJetInputPatternWriter_cfi.py
+++ L1Trigger/L1CaloTrigger/python/l1tS2PFJetInputPatternWriter_cfi.py
@@ -2,11 +2,12 @@
 import FWCore.ParameterSet.Config as cms
 
 l1tS2PFJetInputPatternWriter = cms.EDAnalyzer('L1TS2PFJetInputPatternWriter',
-    inputCollectionTag = cms.InputTag("l1pfCandidates", "Puppi", "IN"),
+    pfTag = cms.InputTag("l1pfCandidates", "Puppi", "IN"),
     filename       = cms.untracked.string("pattern.txt"),
-    nChanPerQuad   = cms.untracked.uint32(4),
-    nQuads         = cms.untracked.uint32(18),
-    nHeaderFrames  = cms.untracked.uint32(1),
-    nPayloadFrames = cms.untracked.uint32(40),
-    nClearFrames   = cms.untracked.uint32(13)
+    outDir       = cms.untracked.string("patterns"),
+    # nChanPerQuad   = cms.untracked.uint32(4),
+    # nQuads         = cms.untracked.uint32(18),
+    nHeaderFrames  = cms.untracked.uint32(0),
+    nPayloadFrames = cms.untracked.uint32(15),
+    nClearFrames   = cms.untracked.uint32(0)
 )
