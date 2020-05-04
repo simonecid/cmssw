739c455b89f8ef8c3473aec2108ecfac370a50d9
Simone Bologna <smnbologna@gmail.com>
2020-05-04 11:14:50 +0100

    updated to match new pw

--- L1Trigger/L1CaloTrigger/python/l1tS2PFJetInputPatternWriter_cfi.py
+++ L1Trigger/L1CaloTrigger/python/l1tS2PFJetInputPatternWriter_cfi.py
@@ -3,11 +3,14 @@ import FWCore.ParameterSet.Config as cms
 
 l1tS2PFJetInputPatternWriter = cms.EDAnalyzer('L1TS2PFJetInputPatternWriter',
     pfTag = cms.InputTag("l1pfCandidates", "Puppi", "IN"),
-    filename       = cms.untracked.string("pattern.txt"),
+    filename       = cms.untracked.string("pattern"),
     outDir       = cms.untracked.string("patterns"),
     # nChanPerQuad   = cms.untracked.uint32(4),
     # nQuads         = cms.untracked.uint32(18),
-    nHeaderFrames  = cms.untracked.uint32(0),
     nPayloadFrames = cms.untracked.uint32(15),
-    nClearFrames   = cms.untracked.uint32(0)
+    framesPerFile = cms.untracked.uint32(1015),
+    evPerFile = cms.untracked.uint32(50)
 )
+
+
+# 50 * 15 = 750, leaving ~ 250 frames to finish processing
